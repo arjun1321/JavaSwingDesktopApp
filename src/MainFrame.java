666667d512ade5f55cd0b85de18ed77be3d9521d
arjun1321 <guptaarjun152@gmail.com>
2016-10-07 20:55:19 +0530

    Custom components adding scrollbar

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -4,31 +4,36 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 
 public class MainFrame extends JFrame {
-	private JTextArea textArea;
+//	private JTextArea textArea;
 	private JButton btn;
+	private TextPanel textPanel;
 	
 	public MainFrame() {
 		
 		super("Hello World");
 		setLayout(new BorderLayout());
 		
-		textArea = new JTextArea();
+//		textArea = new JTextArea();
 		btn = new JButton("Click Me!");
+		textPanel = new TextPanel();
 		
 		btn.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent arg0) {
-				textArea.append("Hello\n");
+//				textArea.append("Hello\n");
+				textPanel.appendText("Hello\n");
 				
 			}
 			
 		});
 		
-		add(textArea, BorderLayout.CENTER);
+//		add(new JScrollPane(textArea), BorderLayout.CENTER);
+		add(textPanel, BorderLayout.CENTER);
 		add(btn, BorderLayout.SOUTH);
 		
 		setSize(600, 500);
