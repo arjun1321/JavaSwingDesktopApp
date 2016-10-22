bf353e907f4a889317e30c0097ee31e21b376820
arjun1321 <guptaarjun152@gmail.com>
2016-10-22 18:54:29 +0530

    CheckBox in menus

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -3,6 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -97,14 +98,25 @@ public class MainFrame extends JFrame {
 		JMenu windowMenu = new JMenu("Window");
 		JMenu showMenu = new JMenu("Show");
 		
-		JMenuItem showFormItem = new JMenuItem("Person Form");
+//		JMenuItem showFormItem = new JMenuItem("Person Form");
+//		showMenu.add(showFormItem);
 		
+		JCheckBoxMenuItem showFormItem = new JCheckBoxMenuItem("Person Form");
+		showFormItem.setSelected(true);
 		showMenu.add(showFormItem);
 		windowMenu.add(showMenu);
 		
 		menuBar.add(fileMenu);
 		menuBar.add(windowMenu);
 		
+		showFormItem.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent arg0) {
+				JCheckBoxMenuItem menuItem = (JCheckBoxMenuItem) arg0.getSource();
+				formPanel.setVisible(menuItem.isSelected());
+			}
+			
+		});
 		
 		return menuBar;
 	}
